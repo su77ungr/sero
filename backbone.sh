07bb106e26dfad01b20d92116287749303144e12
su77ungr <69374354+su77ungr@users.noreply.github.com>
2021-10-11 20:36:08 +0200

    Update backbone.sh

--- backbone.sh
+++ backbone.sh
@@ -8,28 +8,20 @@ while :
     if [ $or = $vertikal1 ] && [ $i = 2 ]
      then
         i=$((i+1))
-        echo Aktion1
         xrandr -o left
-        sleep 1
         python /home/pi/motorcw.py
         z=1
     else
         if [ $or = $horizontal1 ] && [ $z = 1 ] || [ $or = $horizontal2 ] && [ $z = 1 ]
         then
-            echo Aktion2
-           # sudo pkill -9 -f /home/pi/motorcw.py
-           # sleep 1
-           # sudo pkill -9 -f /home/pi/motorcw.py
-           # sudo pkill -9 -f /home/pi/motorcw.py
             xrandr -o normal
-            sleep 1
             sudo python /home/pi/motorccw.py
             z=$((z+1))
         fi
         i=1
                 while :
                  do
-                        or=$(adb shell dumpsys window | grep "mCurRotation" | cut -c 18); sleep 1
+                        or=$(adb shell dumpsys window | grep "mCurRotation" | cut -c 18)
                         if [ $or = $horizontal1 ] || [ $or = $horizontal2 ]
                         then
                             break
