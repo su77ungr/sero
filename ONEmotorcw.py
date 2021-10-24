ede74c7187ede993e9a57798ac097618effe3a49
su77ungr <69374354+su77ungr@users.noreply.github.com>
2021-10-24 14:16:35 +0200

    Update ONEmotorcw.py

--- ONEmotorcw.py
+++ ONEmotorcw.py
@@ -1,9 +1,6 @@
-NU nano 3.2                                                             motorcw.py                                                                        
-
 import RPi.GPIO as GPIO
 import time
 GPIO.setwarnings(False)
-#GPIO.cleanup()
 in1 = 24
 in2 = 23
 en = 25
@@ -14,31 +11,9 @@ GPIO.setup(en,GPIO.OUT)
 p=GPIO.PWM(en,1000)
 p.start(100)
 while True:
-#       u = subprocess.run(["echo 4"], capture_output=True)
-#       for x in range(0, 9000000):
-
-#GPIO.output(in1,GPIO.LOW)
-#GPIO.output(in2,GPIO.LOW)
         GPIO.output(in1,GPIO.HIGH)
         GPIO.output(in2,GPIO.LOW)
         time.sleep(3)
-#try:
-  #  while 1:
-#       time.sleep(2)
-
-#except KeyboardInterrupt:
-#       pass
-#p.stop()
-#GPIO.cleanup()
-        #GPIO.output(in1,GPIO.HIGH)
-        #GPIO.output(in2,GPIO.LOW)
-        #GPIO.cleanup() #       time.sleep(5)
-#finally:
-        #GPIO.cleanup()
-        #GPIO.output(in1,GPIO.HIGH)
-        #GPIO.output(in2,GPIO.LOW)
-        #GPIO.cleanup()
-#output = subprocess.check_output('adb shell dumpsys window | grep "mCurRotation" | cut -c 18', shell=True, text=True)
 
 
 
