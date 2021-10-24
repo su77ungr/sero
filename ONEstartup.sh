b317b9e6ca240cc55a68d1e12976f1429fdc15b2
su77ungr <69374354+su77ungr@users.noreply.github.com>
2021-10-24 14:17:43 +0200

    Update ONEstartup.sh

--- ONEstartup.sh
+++ ONEstartup.sh
@@ -5,13 +5,12 @@ adb tcpip 5555
 adb connect $z
 sleep 3
 ask=$(adb devices | tail -c 3)
-connected="e" #connection etablished
+connected="e"
 x=5
 if [ $ask = $connected ]
         then
                 echo "sucess"
                 konsole -e sudo  /snap/bin/scrcpy --fullscreen --encoder 'OMX.google.h264.encoder' --always-on-top &
-                #sudo /home/pi/geil2.sh &
                 sudo /home/pi/y.sh &
                 sudo /home/pi/backbone.sh &
                 bluetoothctl pair D4:11:A3:22:1C:68
