e5bea416e381a0a781cb269261ad595481d4c0e4
su77ungr <69374354+su77ungr@users.noreply.github.com>
2022-01-27 01:51:23 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,3 +7,5 @@ sudo apt-get install apache2
 curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
 sudo apt-get install nodejs
 curl https://get.telebit.io/ | bash
+sudo npm install localtunnel
+##custom url possible lt --port XY -s XC
